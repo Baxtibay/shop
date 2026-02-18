ea8b5bf250ef10e1e251ed10743c2e81058bb00f
Baxtibay <baxtibaybekniyazov@gmail.com>
2026-02-18 16:56:47 +0500

    Fix case-sensitive App.vue import

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
-import App from './components/app/app.vue'
+import App from './components/app/App.vue'
 import router from './router'
 
 createApp(App)
